e7429438b55849e819b227837fb8ef58bad23c7f
lison <lison.liou@yahoo.com>
2014-07-16 15:42:05 +0800

    finish the listview and sqlite demo

--- src/com/example/helloworld/SqliteActivity.java
+++ src/com/example/helloworld/SqliteActivity.java
@@ -9,14 +9,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.sqlite.db.DatabaseHelper;
@@ -27,7 +30,7 @@ public class SqliteActivity extends Activity {
 			btnQuery;
 	private ListView listviewSqlite;
 	private EditText editText1, editText2;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -35,7 +38,6 @@ public class SqliteActivity extends Activity {
 		super.setContentView(R.layout.sqlite);
 
 		listviewSqlite = (ListView) super.findViewById(R.id.listviewSqlite);
-		listviewSqlite.setAdapter(null);
 
 		btnCreateDatabase = (Button) super.findViewById(R.id.btnCreateDatabase);
 		btnCreateDatabase.setOnClickListener(new btnOnClickListener());
@@ -61,7 +63,78 @@ public class SqliteActivity extends Activity {
 			}
 		});
 	}
+	
+	class MyListViewAdapter extends BaseAdapter
+	{
+		private ArrayList<HashMap<String, String>> hashList = new ArrayList<HashMap<String, String>>();
+		private Activity activity;
+		
+		public MyListViewAdapter(Activity activity,ArrayList<HashMap<String,String>> hash)
+		{
+			super();
+			this.activity=activity;
+			this.hashList=hash;
+		}
 
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return hashList.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			
+			return hashList.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+		public class ViewHolder
+		{
+			TextView txtId;
+			TextView txtDeviceIp;
+			TextView txtDeviceName;
+			TextView txtCreateTime;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			
+			ViewHolder holder;
+			LayoutInflater inflater=activity.getLayoutInflater();
+			
+			if(arg1==null)
+			{
+				arg1=inflater.inflate(R.layout.sqlite_rowitem, null);
+				holder=new ViewHolder();
+				holder.txtId=(TextView)arg1.findViewById(R.id.textViewId);
+				holder.txtDeviceIp=(TextView)arg1.findViewById(R.id.textViewDeviceIp);
+				holder.txtDeviceName=(TextView)arg1.findViewById(R.id.textViewDeviceName);
+				holder.txtCreateTime=(TextView)arg1.findViewById(R.id.textViewCreateTime);
+				
+				arg1.setTag(holder);
+			}
+			else 
+			{
+				holder=(ViewHolder)arg1.getTag();
+			}
+			
+			HashMap<String,String> map=hashList.get(arg0);
+			holder.txtId.setText(map.get("id"));
+			holder.txtDeviceIp.setText(map.get("deviceIp"));
+			holder.txtDeviceName.setText(map.get("deviceName"));
+			holder.txtCreateTime.setText(map.get("createTime"));
+			
+			return arg1;
+		}}
+	
 	class btnOnClickListener implements OnClickListener {
 		@Override
 		public void onClick(View arg0) {
@@ -103,28 +176,38 @@ public class SqliteActivity extends Activity {
 				break;
 			case R.id.btnQuery:
 
-				Cursor cursor = db.query("device", new String[] { "Id", "deviceIp", "deviceName", "createTime" }, "", null, "", "", "createTime desc", "");
+				Cursor cursor = db.query("device", new String[] { "id",
+						"deviceIp", "deviceName", "createTime" }, "", null, "",
+						"", "createTime desc", "");
+				
 				ArrayList<HashMap<String, String>> hashList = new ArrayList<HashMap<String, String>>();
-
 				while (cursor.moveToNext()) {
-					Log.i("sqlite3-->", cursor.getString(cursor.getColumnIndex("id")));
-					Log.i("sqlite3-->", cursor.getString(cursor.getColumnIndex("deviceIp")));
-					Log.i("sqlite3-->", cursor.getString(cursor.getColumnIndex("deviceName")));
-					Log.i("sqlite3-->", cursor.getString(cursor.getColumnIndex("createTime")));
+					Log.i("sqlite3-->",
+							cursor.getString(cursor.getColumnIndex("id")));
+					Log.i("sqlite3-->",
+							cursor.getString(cursor.getColumnIndex("deviceIp")));
+					Log.i("sqlite3-->", cursor.getString(cursor
+							.getColumnIndex("deviceName")));
+					Log.i("sqlite3-->", cursor.getString(cursor
+							.getColumnIndex("createTime")));
 
 					HashMap<String, String> hash1 = new HashMap<String, String>();
-					hash1.put("id", String.valueOf(cursor.getColumnIndex("id")));
-					hash1.put("deviceIp", String.valueOf(cursor.getColumnIndex("deviceIp")));
-					hash1.put("deviceName", String.valueOf(cursor.getColumnIndex("deviceName")));
-					hash1.put("createTime", String.valueOf(cursor.getColumnIndex("createTime")));
+					hash1.put("id", cursor.getString(cursor.getColumnIndex("id")));
+					hash1.put("deviceIp", cursor.getString(cursor.getColumnIndex("deviceIp")));
+					hash1.put("deviceName", cursor.getString(cursor.getColumnIndex("deviceName")));
+					hash1.put("createTime",cursor.getString(cursor.getColumnIndex("createTime")));
 
 					hashList.add(hash1);
 				}
 
-				SimpleAdapter adapter = new SimpleAdapter(SqliteActivity.this,
-						hashList, R.layout.sqlite_rowitem, new String[] { "id", "deviceIp", "deviceName", "createTime" },
-						new int[] { R.id.textViewId, R.id.textViewDeviceIp, R.id.textViewDeviceName, R.id.textViewCreateTime });
+//				SimpleAdapter adapter = new SimpleAdapter(SqliteActivity.this,
+//						hashList, R.layout.sqlite_rowitem, new String[] { "id",
+//								"deviceIp", "deviceName", "createTime" },
+//						new int[] { R.id.textViewId, R.id.textViewDeviceIp,
+//								R.id.textViewDeviceName,
+//								R.id.textViewCreateTime });
 				
+				MyListViewAdapter adapter=new MyListViewAdapter(SqliteActivity.this,hashList);
 				listviewSqlite.setAdapter(adapter);
 
 				break;
